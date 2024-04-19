ee7bd5c3cfc849794f2484a713223bafc607c37d
Navjot <navjotcheema289@gmail.com>
2024-04-19 16:49:21 -0500

    Image resizing implemented

--- app/uploaders/product_image_uploader.rb
+++ app/uploaders/product_image_uploader.rb
@@ -29,10 +29,4 @@ class ProductImageUploader < CarrierWave::Uploader::Base
       %w(jpg jpeg png)
     end
   
-    # Override the filename of the uploaded files:
-    # Avoid using model.id or version_name here, see uploader/store.rb for details.
-    # def filename
-    #   "something.jpg" if original_filename
-    # end
-  
   end
